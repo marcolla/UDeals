2a3f039fbc77e47c33f974bd0cdd02e5e3b3f317
daniel liang <dliang@udel.edu>
2017-11-29 15:58:10 -0500

    stuff

--- backend/controllers/udeals.controller.js
+++ backend/controllers/udeals.controller.js
@@ -8,15 +8,28 @@ exports.getUDeals=function(req,res,next){
 
 exports.createUDeal=function(req,res,next){
     console.log(req.body);
-    var u=new UDeal({ offer: 'Half Priced Full Size Macs', details: {name:'Mad Macs and Matildas', addresss: 'main street',
-    day: 'Monday',
-    time: 'all day',
-    description: '50% off full sized macs (lobster mac excluded from this deal)',
-    deliver: false,
-    link: 'www.madmacs.com maybe',
-    recurring: true }});
+    var u=new UDeal({offer: req.body.offer,
+        details:{name:req.body.details.name,
+            address:req.body.details.address,
+            day: req.body.details.day,
+            time: req.body.details.time,
+            description: req.body.details.description,
+            deliver: req.body.details.deliver,
+            link: req.body.details.link,
+            recurring: req.body.details.recurring}});
+    // var u=new UDeal({ offer: '3 dolla ice cream', details: {name:'qdoba', addresss: 'main street',
+    // day: 'Sunday',
+    // time: '1pm - 3am',
+    // description: 'I scream for ice cream',
+    // deliver: false,
+    // link: 'www.qdoba.org perhaps',
+    // recurring: true }});
 
     u.save(function(err,udeal){
         res.status(200).json(udeal);
     });
+}
+
+exports.findUDeal=function(req,res,next){
+    
 }
