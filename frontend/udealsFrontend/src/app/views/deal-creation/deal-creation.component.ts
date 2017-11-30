fc71199d3519d4dcd6955c3bac5fd5b33a1d22df
Richard <rgantt@udel.edu>
2017-11-30 18:58:57 -0500

    dropbox on newdeal page

--- frontend/udealsFrontend/src/app/views/deal-creation/deal-creation.component.ts
+++ frontend/udealsFrontend/src/app/views/deal-creation/deal-creation.component.ts
@@ -8,7 +8,7 @@ import { UdealsService } from '../../modules/udeals/udeals.service';
 })
 export class DealCreationComponent implements OnInit {
   _apiSVC: UdealsService;
-  
+
   constructor() { }
 
   ngOnInit() {
@@ -33,4 +33,28 @@ export class DealCreationComponent implements OnInit {
     });
   }
 
+  myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  filterFunction() {
+    var input, filter, ul, li, a, i, div;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("myDropdown");
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+        if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";
+        } else {
+            a[i].style.display = "none";
+        }
+    }
+  }  
+
+
+
+
 }
+
+
